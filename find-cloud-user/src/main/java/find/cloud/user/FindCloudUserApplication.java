4cba762c48cb8fd52f151ed2354540f4d110bb40
turnglight <335771269@qq.com>
2020-10-23 10:57:13 +0800

    提交

---
+++ find-cloud-user/src/main/java/find/cloud/user/FindCloudUserApplication.java
@@ -0,0 +1,13 @@
+package find.cloud.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindCloudUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindCloudUserApplication.class, args);
+	}
+
+}
