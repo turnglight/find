d461c9722976a4298a819639cc4bbfcda8b91f04
“Turne <335771269@qq.com>
2021-07-26 09:10:19 +0800

    框架代码提交

--- find-cloud-gateway/src/main/java/find/cloud/gateway/FindCloudGatewayApplication.java
+++ find-cloud-gateway/src/main/java/find/cloud/gateway/FindCloudGatewayApplication.java
@@ -2,16 +2,15 @@ package find.cloud.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FindCloudGatewayApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext configurableApplicationContext = SpringApplication.run(FindCloudGatewayApplication.class, args);
-		String userName = configurableApplicationContext.getEnvironment().getProperty("user.name");
-		String userAge = configurableApplicationContext.getEnvironment().getProperty("user.age");
-		System.err.println("user name :"+userName+"; age: "+userAge);
+		SpringApplication.run(FindCloudGatewayApplication.class, args);
 	}
 
 }
